239be33157473395e00d703127ccf06460800932
Mahdi Farhang <mafarhang76@gmail.com>
2023-05-22 21:07:47 +0330

    Market list page

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from core.views import *
 app_name='core'
 
 urlpatterns = [
+    path('market-list', market_list, name='market_list'),
     path('load-orderbook/<str:market>', load_orderbook, name='load_orderbook'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from django.template.response import TemplateResponse
 from core.tasks import load_market_orderbook
+
+
+# Todo: Handle Permission
+def market_list(request):
+    market_list = [
+        'USDTIRT',
+        'BTCIRT',
+        'BTCUSDT',
+    ]
+    return TemplateResponse(request=request, template="core/market_list.html", status=200,
+                            context={'markets': market_list})
+
 
 # Todo: Handle permissions
 def load_orderbook(request, market):
