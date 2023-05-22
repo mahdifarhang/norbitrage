2eed06ddf4344ff0d3e7d464f3938385722b5e3d
Mahdi Farhang <mafarhang76@gmail.com>
2023-05-22 20:22:44 +0330

    Write a Requester class for all future requesting to Nobitex.

--- core/requester.py
+++ core/requester.py
@@ -1,7 +1,83 @@
 from django.conf import settings
 
+import requests
+import json
+from urllib.parse import urljoin
+import logging
 
-class Requester():
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+class APIRequester(object):
+    """
+    This base request can be instantiated with a base URL for an API.
+    Subsequent calls can be made with the usual arguments for :meth:`requests.request`, specifying only the API route::
+        >>> api_request = APIRequest('http://example.com/api/v3')
+        >>> response = api_request('POST', '/login', data={'username': 'foo', 'password': 'bar'})
+    etc.
+    """
     base_url = getattr(settings, 'NOBITEX_API_BASE_URL', 'https://api.nobitex.ir/')
-    endpoint = None
 
+    def __init__(self, base_url=None, headers=None, json_response=True):
+        self.json_response = json_response
+        if base_url is not None:
+            self.base_url = base_url
+        if not self.base_url.endswith('/'):
+            self.base_url += '/'
+        if headers is not None:
+            self.headers = headers
+        else:
+            self.headers = {}
+            if self.base_url == getattr(settings, 'NOBITEX_API_BASE_URL'):
+                self.headers.update({
+                    'Authorization': f"Token {getattr(settings, 'NOBITEX_AUTHORIZATION_TOKEN')}"
+                })
+
+    def __call__(self, endpoint, method="GET", **kwargs):
+        self.endpoint = endpoint
+        if self.endpoint.startswith('/'):
+            self.endpoint = self.endpoint[1:]
+        if not self.base_url.endswith('/'):
+            self.base_url += '/'
+
+        self.url = urljoin(self.base_url, self.endpoint, allow_fragments=False)
+        headers = kwargs.pop('headers', {})
+        self.headers.update(headers)
+
+        response = requests.request(method=method, url=self.url, headers=headers, **kwargs)
+        if response.status_code // 100 == 2:
+            if self.json_response:
+                return json.loads(response.text)
+            return response.text
+        else:
+            raise response
+        # if 'data' in kwargs:
+        #     log.info(u'{} {} with headers:\n{}\nand data:\n{}'.format(
+        #         method,
+        #         self.url,
+        #         json.dumps(headers, indent=4),
+        #         json.dumps(kwargs['data'], indent=4)
+        #     ))
+        # elif 'json' in kwargs:
+        #     log.info(u'{} {} with headers:\n{}\nand JSON:\n{}'.format(
+        #         method,
+        #         self.url,
+        #         json.dumps(headers, indent=4),
+        #         json.dumps(kwargs['json'], indent=4)
+        #     ))
+        # else:
+        #     log.info(u'{} {} with headers:\n{}'.format(
+        #         method,
+        #         self.url,
+        #         json.dumps(headers, indent=4)
+        #     ))
+        #
+        # log.info(
+        #     u'Response to {} {} => {} {}\n{}'.format(
+        #         method,
+        #         self.url,
+        #         response.status_code,
+        #         response.reason,
+        #         response.text[:100])
+        # )
