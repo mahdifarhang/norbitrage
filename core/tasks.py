a291e899a51a6f1f4f69469feb695541de9b10f4
Mahdi Farhang <mafarhang76@gmail.com>
2023-06-20 20:13:51 +0330

    Better log and calculation of price difference

--- core/tasks.py
+++ core/tasks.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from celery import shared_task
 
 from core.requester import APIRequester
+from core.telegram import send_telegram_message
 from django.core.cache import cache
 
 COIN_LIST = [
@@ -48,31 +49,43 @@ def check_coin(coin):
     irt_response = requester(f'v2/orderbook/{coin}IRT')
     usdtirt_price = get_USDTIRT_price() or '0'
     usdt_bids = usdt_response.get('bids')
-    irt_bids = irt_response.get('bids')
     usdt_asks = usdt_response.get('asks')
+
+    irt_bids = irt_response.get('bids')
     irt_asks = irt_response.get('asks')
+
     buy_in_usdt_sell_in_irt = {
         'buy_price': Decimal(usdt_bids[0][0]),
         'sell_price': Decimal(irt_asks[0][0]) / Decimal(usdtirt_price),
+        'amount': min(Decimal(usdt_bids[0][1]), Decimal(irt_asks[0][1])),
     }
     buy_in_irt_sell_in_usdt = {
         'buy_price': Decimal(irt_bids[0][0]),
         'sell_price': Decimal(usdt_asks[0][0]) * Decimal(usdtirt_price),
+        'amount': min(Decimal(irt_bids[0][1]), Decimal(usdt_asks[0][1])),
     }
 
+    text = ''
+    chat_id = getattr(settings, 'TELEGRAM_CHAT_ID')
     if buy_in_usdt_sell_in_irt['buy_price'] < buy_in_usdt_sell_in_irt['sell_price']:
         text = f"___________{coin}___________\n" \
                f"profits: USDT -> IRT\n" \
-               f"Buy Price: {buy_in_usdt_sell_in_irt['buy_price']}\n" \
-               f"Sell Price: {buy_in_usdt_sell_in_irt['sell_price']}\n"
+               f"Amount: {buy_in_usdt_sell_in_irt['amount']}\n" \
+               f"Buy Price: {buy_in_usdt_sell_in_irt['buy_price']} USDT\n" \
+               f"Sell Price: {buy_in_usdt_sell_in_irt['sell_price']} USDT\n" \
+               f"USDTIRT price: {Decimal(usdtirt_price)}\n" \
+               f"Difference Percentage: {(buy_in_usdt_sell_in_irt['sell_price'] - buy_in_usdt_sell_in_irt['buy_price']) / buy_in_usdt_sell_in_irt['buy_price'] * 100}%\n"
         print(text)
-        # send_telegram.delay(text)
+        send_telegram_message_task.delay(text, chat_id)
 
     if buy_in_irt_sell_in_usdt['buy_price'] < buy_in_irt_sell_in_usdt['sell_price']:
         text = f"___________{coin}___________\n" \
-               f"profits: IRT -> USDT\n" \
-               f"Buy Price: {buy_in_irt_sell_in_usdt['buy_price']}\n" \
-               f"Sell Price: {buy_in_irt_sell_in_usdt['sell_price']}\n"
+               f"direction: IRT -> USDT\n" \
+               f"Amount: {buy_in_irt_sell_in_usdt['amount']}\n" \
+               f"Buy Price: {buy_in_irt_sell_in_usdt['buy_price']} IRT\n" \
+               f"Sell Price: {buy_in_irt_sell_in_usdt['sell_price']} IRT\n" \
+               f"USDTIRT price: {Decimal(usdtirt_price)}\n" \
+               f"Difference Percentage: {(buy_in_irt_sell_in_usdt['sell_price'] - buy_in_irt_sell_in_usdt['buy_price']) / buy_in_irt_sell_in_usdt['buy_price'] * 100}%\n"
         print(text)
         # send_telegram.delay(text)
 
